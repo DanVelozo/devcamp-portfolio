1b65920f97599f1e97727ec4b5fc6344fabebb1d
Danilo Velozo <velozo.dan@gmail.com>
2017-07-26 15:23:51 -0300

    Integrated friendly routes for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
-  get 'contact', to: 'pages/contact'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
 
